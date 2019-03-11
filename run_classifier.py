cfe43e784004936f568c725a3b78d32cb0042077
Ryan Almodovar <ralmodovar@salesforce.com>
2019-03-10 19:26:02 -0700

    modify hyperparams

--- run_classifier.py
+++ run_classifier.py
@@ -75,13 +75,17 @@ flags.DEFINE_bool(
     "do_predict", False,
     "Whether to run the model in inference mode on the test set.")
 
-flags.DEFINE_integer("train_batch_size", 32, "Total batch size for training.")
+# MODIFIED: 32
+flags.DEFINE_integer("train_batch_size", 64, "Total batch size for training.")
 
-flags.DEFINE_integer("eval_batch_size", 8, "Total batch size for eval.")
+# MODIFIED: 8
+flags.DEFINE_integer("eval_batch_size", 16, "Total batch size for eval.")
 
-flags.DEFINE_integer("predict_batch_size", 8, "Total batch size for predict.")
+# MODIFIED: 8
+flags.DEFINE_integer("predict_batch_size", 16, "Total batch size for predict.")
 
-flags.DEFINE_float("learning_rate", 5e-5, "The initial learning rate for Adam.")
+# MODIFIED: 5e-5
+flags.DEFINE_float("learning_rate", 1e-5, "The initial learning rate for Adam.")
 
 flags.DEFINE_float("num_train_epochs", 3.0,
                    "Total number of training epochs to perform.")
